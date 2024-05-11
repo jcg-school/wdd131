06e3c5a341d327d7ddd7028aca8701d1c422acd4
jcg-school <jcaelynschool@outlook.com>
2024-05-11 17:10:18 -0400

    starting push

---
+++ scripts/place.js
@@ -0,0 +1,8 @@
+const currentYearElementSelector = document.getElementById('currentyear');
+const currentYearVariable = new Date().getFullYear();
+currentYearElementSelector.innerHTML = currentYearVariable;
+
+const lastModifiedDateElementSelector = document.getElementById('lastmodified');
+const lastModifiedDate = new Date(document.lastModified);
+const formattedLastModifiedDate = `${lastModifiedDate.toLocaleDateString()} ${lastModifiedDate.toLocaleTimeString()}`;
+lastModifiedDateElementSelector.textContent = formattedLastModifiedDate;
