6b61c1c3592ae85a3868f01191f20674cb7cc0de
jcg-school <jcaelynschool@outlook.com>
2024-06-05 17:19:33 -0400

    edit

--- project/scripts/dicetray.js
+++ project/scripts/dicetray.js
@@ -1,6 +1,13 @@
+const currentYearElementSelector = document.getElementById('currentyear');
+const currentYearVariable = new Date().getFullYear();
+currentYearElementSelector.innerHTML = currentYearVariable;
+
+const lastModifiedDateElementSelector = document.getElementById('lastmodified');
+const lastModifiedDate = new Date(document.lastModified);
+const formattedLastModifiedDate = `${lastModifiedDate.toLocaleDateString()} ${lastModifiedDate.toLocaleTimeString()}`;
+lastModifiedDateElementSelector.textContent = formattedLastModifiedDate;
 
 // dice tray functionality
-
 const diceElements = document.querySelectorAll('.dice');
 const rollCountElement = document.getElementById('roll-count');
 const rollTotalElement = document.getElementById('roll-total');
