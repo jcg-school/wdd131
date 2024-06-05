f4ee9c1360674ee8c4db49e862c13854997ebbf0
jcg-school <131555073+jcg-school@users.noreply.github.com>
2024-06-05 10:14:50 -0400

    form-review edit

--- scripts/form-review.js
+++ scripts/form-review.js
@@ -1,4 +1,7 @@
 
+  document.getElementById('currentyear').textContent = new Date().getFullYear();
+  document.getElementById('lastmodified').textContent = document.lastModified;
+
 function updateReviewCount() {
     let numReviews = Number(window.localStorage.getItem("numReviews-ls")) || 0;
     document.querySelector(".posted-reviews").textContent = numReviews;
